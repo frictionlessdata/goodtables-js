6c6c7ef096bdc3777363c19b3fde1819cae8f30d
roll <eskarev@gmail.com>
2017-07-02 23:05:28 +0200

    Fixed ApiClient export in index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require('babel-polyfill')
+const {ApiClient} = require('./client')
 const {validate} = require('./validate')
 const spec = require('./spec.json')
 
 
 // Module API
 
+module.exports.ApiClient = ApiClient
 module.exports.validate = validate
 module.exports.spec = spec
